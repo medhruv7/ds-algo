ccbee6ae016d2d5a220023a71b26649b71bf6954
x <x@xs-MacBook-Pro.local>
2025-11-29 08:45:28 +0530

    added fenwick tree

---
+++ algos/fenwick_tree.cpp
@@ -0,0 +1,57 @@
+// #include "common_headers.h"
+#include "bits/stdc++.h"
+
+using namespace std;
+using ll = long long;
+
+struct Fenwick_tree {
+    int n;
+    vector<ll> tree;
+
+    Fenwick_tree(int n) : n(n), tree(n + 1, 0) {};
+
+    Fenwick_tree(vector<int> &arr) : n(arr.size()) , tree(arr.size() + 1) {
+        for(int i = 0;i < n; ++i) {
+            update(i, arr[i]);
+        }
+    };
+
+    // update in [1, i]
+    void update(int i, ll delta) {
+        // convert to 1 indexed
+        i++;
+        while(i <= n) {
+            tree[i] += delta;
+            // add last 1 bit significant bit
+            // update all the segments that have responsibility of this cell
+            i += (i&-i);
+        }
+    }
+
+    // query from [1, n]
+    ll query(int r) {
+        // convert to 1 indexed
+        r++;
+        ll ans = 0;
+        while(r >= 1) {
+            ans += tree[r];
+            // move to prev parts of the tree where prev segments have their responsibility
+            r -= (r&-r);
+        }
+
+        return ans;
+    }
+
+    ll query(int l, int r) {
+        if(l == 0) {
+            return query(r);
+        }
+
+        return (query(r) - query(l - 1));
+    }
+};
+
+int main() {
+    
+    return 0;
+}
